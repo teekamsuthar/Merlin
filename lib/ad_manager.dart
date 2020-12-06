1a9640fe2d9b4462a20f17e41c44f28f766795a5
teekamsuthar <teekam.suthar1@gmail.com>
2020-12-06 13:56:43 +0530

    updated ad ids

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-7232818117062807~4082355280";
-      return "ca-app-pub-3940256099942544~4354546703"; //test id
+      return "ca-app-pub-7232818117062807~4082355280";
+      // return "ca-app-pub-3940256099942544~4354546703"; //test id
     } else if (Platform.isIOS) {
       return "ca-app-pub-7232818117062807~5450782438";
     } else {
@@ -14,8 +14,8 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-7232818117062807/2357715232";
-      return "ca-app-pub-3940256099942544/8865242552"; // test id
+      return "ca-app-pub-7232818117062807/2357715232";
+      // return "ca-app-pub-3940256099942544/8865242552"; // test id
     } else if (Platform.isIOS) {
       return "ca-app-pub-7232818117062807/2365114781";
     } else {
@@ -25,8 +25,8 @@ class AdManager {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-7232818117062807/9801357883";
-      return "ca-app-pub-3940256099942544/7049598008"; //testid
+      return "ca-app-pub-7232818117062807/9801357883";
+      // return "ca-app-pub-3940256099942544/7049598008"; //testid
     } else if (Platform.isIOS) {
       return "ca-app-pub-7232818117062807/9332592204";
     } else {
